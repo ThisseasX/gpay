b6456fad5c5eb41871d4d820669bc5ff88138591
Thisseas X <thisseasx@yahoo.com>
2020-01-22 21:36:27 +0200

    Updates

---
+++ precache-manifest.7e468d0822d96980aaa38d00d973b871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e8e0bf616b9313e78c958998ebede5",
+    "url": "/gpay/index.html"
+  },
+  {
+    "revision": "a9aad26506b7e27e5045",
+    "url": "/gpay/static/js/2.1f8c5f64.chunk.js"
+  },
+  {
+    "revision": "a068657e4f451d28c30deb405f0ebadc",
+    "url": "/gpay/static/js/2.1f8c5f64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f46d080c9370f89d1d3c",
+    "url": "/gpay/static/js/main.2fa1f463.chunk.js"
+  },
+  {
+    "revision": "9032ac247bcde2a8f41c",
+    "url": "/gpay/static/js/runtime-main.477d7c57.js"
+  }
+]);
